0c938e2c194d5a3f83250e539e7b239553cdb477
guilherme daniel <gui.dani13@gmail.com>
2023-02-17 10:47:17 -0300

    criado componente de grid

---
+++ src/components/CustomGrid.tsx
@@ -0,0 +1,62 @@
+import { TPopularMovie } from "@/types/popular_movie";
+import {
+  Bookmark,
+  BookmarkBorder,
+  Favorite,
+  FavoriteBorder,
+} from "@mui/icons-material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Checkbox,
+  Grid,
+  Typography,
+} from "@mui/material";
+
+type Props = {
+  data: TPopularMovie[];
+};
+
+const CustomGrid = ({ data }: Props) => {
+  return (
+    <Grid container spacing={2} mb={4}>
+      {data?.map((item: TPopularMovie) => (
+        <Grid item key={item.id} xs={6} sm={3} md={2}>
+          <Card>
+            <CardMedia
+              component="img"
+              width="100%"
+              image={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
+            />
+            <CardContent>
+              <Typography>{item.title}</Typography>
+            </CardContent>
+            <CardActions>
+              <Checkbox
+                icon={<FavoriteBorder />}
+                checkedIcon={<Favorite />}
+                onClick={() => console.log("favoritado")}
+              />
+              <Checkbox
+                icon={<BookmarkBorder />}
+                checkedIcon={<Bookmark />}
+                onClick={() => console.log("Assistir mais tarde")}
+              />
+              <Checkbox
+                icon={<CheckCircleOutlineIcon />}
+                checkedIcon={<CheckCircleIcon />}
+                onClick={() => console.log("asd")}
+              />
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default CustomGrid;
---
+++ src/types/popular_movie.ts
@@ -0,0 +1,8 @@
+export type TPopularMovie = {
+  id: number;
+  title: string;
+  overview: string;
+  release_date: string;
+  vote_average: number;
+  poster_path: string;
+};
